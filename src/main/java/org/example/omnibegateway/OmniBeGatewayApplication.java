16139dd9e0d17dde52af2c9e3fbbef88bc562303
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-04-08 14:00:20 +0900

    feat: 초기 프로젝트 및 CI-CD 구성

---
+++ src/main/java/org/example/omnibegateway/OmniBeGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.omnibegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniBeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmniBeGatewayApplication.class, args);
+    }
+
+}
